b48f50f149c7c00d15575e2dec6ceed7f297faaf
Danny Brown <dannywb@gmail.com>
2020-05-19 19:43:21 -0400

    Updated js.js file to remove crap

--- public/js/js.js
+++ public/js/js.js
@@ -1,5 +1,3 @@
-console.log('JS file loaded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#message1')
